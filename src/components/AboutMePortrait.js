a2fafa6f0545001e5891556b4028849c3dc725cc
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-05-03 14:17:23 +0200

    added animation

--- src/components/AboutMePortrait.js
+++ src/components/AboutMePortrait.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import portrait from "../assets/portrait.jpg";
 import styled from "styled-components";
+import gsap from "gsap/dist/gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const AboutMePortrait = () => {
+  useEffect(() => {
+    gsap.set(".portrait-box", { bottom: 0, right: 0 });
+
+    ScrollTrigger.matchMedia({
+      "(min-width: 1100px)": function () {
+        ScrollTrigger.create({
+          trigger: ".portrait",
+          start: "top center",
+          end: "bottom center",
+
+          onEnter: () =>
+            gsap.to(".portrait-box", {
+              duration: 1,
+              bottom: "-20px",
+              right: "-20px",
+            }),
+
+          onEnterBack: () =>
+            gsap.to(".portrait-box", {
+              duration: 1,
+              bottom: "-20px",
+              right: "-20px",
+            }),
+
+          onLeave: () =>
+            gsap.to(".portrait-box", { duration: 1, bottom: 0, right: 0 }),
+
+          onLeaveBack: () =>
+            gsap.to(".portrait-box", { duration: 1, bottom: 0, right: 0 }),
+        });
+      },
+    });
+  }, []);
+
   return (
     <Wrapper>
-      <div className="portrait"></div>
+      <div className="portrait">
+        <img src={portrait} alt="portrait" />
+        <div className="portrait-box"></div>
+      </div>
     </Wrapper>
   );
 };
@@ -21,18 +62,20 @@ const Wrapper = styled.section`
     height: 80%;
     background-color: plum;
     position: relative;
+    z-index: 1;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
 
-  .portrait::after {
-    content: "";
+  .portrait-box {
     position: absolute;
-    display: block;
-    border: solid 2px dodgerblue;
     width: 100%;
     height: 100%;
-    bottom: -20px;
-    right: -20px;
     z-index: -1;
+    border: solid 3.5px dodgerblue;
   }
 `;
 
